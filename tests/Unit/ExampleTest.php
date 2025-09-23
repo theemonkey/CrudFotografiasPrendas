7dfe1878c533fa8982f4e13c55115c5c80a02e31
William Aguirre <135067652+theemonkey@users.noreply.github.com>
2025-09-23 07:20:43 -0500

    Update ExampleTest.php

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\TestCase;
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+  /*  public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
     }
